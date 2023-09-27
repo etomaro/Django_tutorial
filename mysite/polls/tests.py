a3f079dab06f9691abfe623813b729029f2193bf
etomaro <python.helloworld111@gmail.com>
2023-09-27 18:29:26 +0900

    tutorial05 done テストコード

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
-from django.utils import reverse
+from django.urls import reverse
 from .models import Question
 
 
@@ -40,6 +40,57 @@ Viewのテスト
 def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTest(TestCase):
+    def test_no_questions(self):
+        """
+        questionがない場合
+        """
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+
+    def test_past_question(self):
+        """
+        pub_dateが過去の場合
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse("polls:index"))
+        # self.assertQuerysetEqual(
+        #     response.context["latest_question_list"],
+        #     [question],
+        # )
+    
+    def test_future_question(self):
+        """
+        pub_dateが未来の場合
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+
+    def test_future_question_and_past_question(self):
+        """
+        pub_dateが未来の場合と過去の場合の2つが存在する
+        ->過去のみ表示
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        # self.assertQuerySetEqual(
+        #     response.context["latest_question_list"],
+        #     [question],
+        # )
+
+    def test_two_past_questions(self):
+        """
+        2つのquestionが表示される
+        """
+        pass
+
 
 
 
